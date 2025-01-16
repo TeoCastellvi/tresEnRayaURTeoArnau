795913a2c68b4a9d8ff23afc1d0b070629b504dd
arnau.casas.7e7 <arnau.casas.7e7@itb.cat>
2025-01-16 16:27:41 +0100

    initial commit

--- app/src/main/java/com/example/tresenrayarobot/MainActivity.kt
+++ app/src/main/java/com/example/tresenrayarobot/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tresenrayarobot
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,34 +15,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.tresenrayarobot.ui.theme.TresEnRayaRobotTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TresEnRayaRobotTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TresEnRayaRobotTheme {
-        Greeting("Android")
     }
 }
