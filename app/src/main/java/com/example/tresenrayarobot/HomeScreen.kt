9764448dc12f54eef4204ef2276fd731c3993908
teo.castellvi.7e7 <teo.castellvi.7e7@itb.cat>
2025-01-16 17:06:26 +0100

    f

--- app/src/main/java/com/example/tresenrayarobot/HomeScreen.kt
+++ app/src/main/java/com/example/tresenrayarobot/HomeScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
@@ -22,100 +24,88 @@ import androidx.compose.ui.unit.sp
 fun HomeScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround
+        horizontalAlignment = Alignment.CenterHorizontally, // Centra la columna horizontalmente
+        verticalArrangement = Arrangement.Center // Centra el contenido verticalmente
     ) {
         Text(
             text = "Tres en ratlla",
-            fontSize = 30.sp,
+            fontSize = 50.sp,
             color = Color.DarkGray,
         )
+        Spacer(modifier = Modifier.height(116.dp))
 
-
-        Spacer(modifier = Modifier.height(40.dp))
-
-        Column() {
-            Row() {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Fila 1
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
             }
-            Row() {
+
+            // Fila 2
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
-
+                )
             }
-            Row() {
+
+            // Fila 3
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
+                )
                 Box(
                     modifier = Modifier
-                        .height(20.dp)
-                        .width(20.dp)
+                        .size(80.dp)
                         .background(Color.Blue)
-                ) {
-
-                }
-
+                )
             }
         }
     }
-
 }
