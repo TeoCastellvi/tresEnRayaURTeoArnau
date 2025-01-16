b6e183ebdcc0c73c3a9053d275c04fd8b55992fd
teo.castellvi.7e7 <teo.castellvi.7e7@itb.cat>
2025-01-16 16:26:39 +0100

    f

--- app/src/main/java/com/example/tresenrayarobot/HomeScreen.kt
+++ app/src/main/java/com/example/tresenrayarobot/HomeScreen.kt
@@ -1,5 +1,23 @@
 package com.example.tresenrayarobot
 
-fun HomeScreen(){
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
+@Composable
+fun HomeScreen(){
+    Column(
+        modifier = Modifier
+    ){
+        Text(
+            text = "Tres en ratlla",
+            fontSize = 30.sp,
+            color = Color.Gray,
+        )
+
+    }
 }
--- app/src/main/java/com/example/tresenrayarobot/Routes.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tresenrayarobot
-
-class Routes {
-}
